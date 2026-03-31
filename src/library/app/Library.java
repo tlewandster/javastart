e551b4998ba46a6ebc745c15b663b3bac15eff12
Tomasz Lewandowski <tlewandster@gmail.com>
2026-03-31 18:38:29 +0200

    Update `Book` info formatting to handle optional ISBN and bump library version to 0.6

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -4,7 +4,7 @@ import library.model.Book;
 
 class Library {
     static void main() {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion", "9788324620845");
