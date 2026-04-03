388d144007076add3facafc0af4a590c64c63465
Tomasz Lewandowski <tlewandster@gmail.com>
2026-04-03 21:53:55 +0200

    Add `Main` class to calculate and display sum of user-input numbers using a loop

---
+++ src/t019_Petle/e1/Main.java
@@ -0,0 +1,18 @@
+package t019_Petle.e1;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ile liczb chcesz wprowadzić?:");
+        double sum = 0;
+        int numberOfNumbers = scanner.nextInt();
+        while (numberOfNumbers-- > 0) {
+            System.out.println("Podaj liczbę: ");
+            sum += scanner.nextInt();
+        }
+        System.out.println("Suma liczb: " + sum);
+        scanner.close();
+    }
+}
