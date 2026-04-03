3ed5f76158c1403629d34c995cc90ac337e4a2d6
Tomasz Lewandowski <tlewandster@gmail.com>
2026-04-03 23:25:39 +0200

    Add `Thermostat` class to simulate temperature adjustment using user input

---
+++ src/t019_Petle/e3/Thermostat.java
@@ -0,0 +1,22 @@
+package t019_Petle.e3;
+
+import java.util.Scanner;
+
+public class Thermostat {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj temperaturę aktualną:");
+        double currentTemperature = scanner.nextDouble();
+        System.out.println("Podaj temperaturę docelową:");
+        double targetTemperature = scanner.nextDouble();
+        while (currentTemperature != targetTemperature) {
+            System.out.println("Aktualna temperatura: " + currentTemperature + " stopni");
+            if (currentTemperature < targetTemperature) {
+                currentTemperature += 0.5;
+            } else {
+                currentTemperature -= 0.5;
+            }
+        }
+        System.out.println("Osiągnięto temperaturę docelową " + currentTemperature + " stopni");
+    }
+}
