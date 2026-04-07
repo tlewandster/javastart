2877455dd6010c7e3038b4eecf00ffeb8ee06b5b
Tomasz Lewandowski <tlewandster@gmail.com>
2026-04-07 20:47:00 +0200

    Add `EvenAdder` class to generate, reverse, and display arrays of even numbers using static methods

---
+++ src/t22_Debugowanie/e1/EvenAdder.java
@@ -0,0 +1,51 @@
+package t22_Debugowanie.e1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class EvenAdder {
+    static void main() {
+        int[] array = createEvenArray(5);
+        array = reverseArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * @param size - number of elements to read
+     * @return array containing even numbers
+     */
+    private static int[] createEvenArray(int size) {
+        Scanner scanner = new Scanner(System.in);
+        int[] result = new int[size];
+        for (int i = 0; i < result.length; i++) {
+            System.out.print("Podaj kolejną liczbę: ");
+            int next = scanner.nextInt();
+            if (isEven(next))
+                result[i] = next;
+            else
+                i--;
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param number - number to be checked
+     * @return true if the number is even, or false otherwise
+     */
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    /**
+     * @param array - array to be reversed
+     * @return - reversed array
+     */
+    private static int[] reverseArray(int[] array) {
+        int[] reversed = new int[array.length];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = array[array.length-1-i];
+        }
+        return reversed;
+    }
+}
