1e8d5d3e2903359b5e596bf28a72fe3032db3be5
Tomasz Lewandowski <tlewandster@gmail.com>
2026-04-08 20:04:29 +0200

    Refactor `Book` and `Magazine` to inherit from `Publication`; streamline constructors and improve code reuse

--- src/library/model/Book.java
+++ src/library/model/Book.java
@@ -9,9 +9,7 @@ public class Book extends Publication {
     // Konstruktory
     public Book(String title, String author, int year, int pages, String publisher,
                 String isbn) {
-        setTitle(title);
-        setPublisher(publisher);
-        setYear(year);
+        super(title, publisher, year);
         this.pages = pages;
         this.author = author;
         this.isbn = isbn;
